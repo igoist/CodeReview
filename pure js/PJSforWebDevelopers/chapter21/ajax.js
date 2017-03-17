b01c8d40fd153e45678ca0339a87bf85c85d8cb7
XuTianZhu <493512132@qq.com>
2017-03-17 23:12:52 +0800

    more details at demo like todolist, those imgs

---
+++ pure js/PJSforWebDevelopers/chapter21/ajax.js
@@ -0,0 +1,28 @@
+var start_time = Date.now();
+
+/* ======== Main: Object ======== */
+
+console.log(typeof XMLHttpRequest);
+
+
+function addURLParam(url, name, value) {
+  url += (url.indexOf("?") == -1 ? "?" : "&");
+  url += encodeURIComponent(name) + "=" + encodeURIComponent(value);
+  return url;
+}
+
+
+
+
+
+
+
+
+/* ======== Main End ======== */
+
+var stop_time = Date.now();
+
+console.log("this script last: " + (stop_time - start_time) + "ms");
+
+start_time = null;
+stop_time = null;
