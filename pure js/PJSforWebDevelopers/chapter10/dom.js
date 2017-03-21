9f11e51cbefd3a1952e8484d52d8a341d6e8d991
XuTianZhu <493512132@qq.com>
2017-03-21 22:46:50 +0800

    看了这么就还没看到 querySelector ...

--- pure js/PJSforWebDevelopers/chapter10/dom.js
+++ pure js/PJSforWebDevelopers/chapter10/dom.js
@@ -12,3 +12,22 @@ var u1 = document.getElementById("u1");
 
 // the right suppose   "↵    "   childElementCount is different with childNodes.length
 console.log("u1.childNodes.length: " + u1.childNodes.length);
+
+function outputAttributes(element) {
+  var pairs = new Array(),
+      attrName,
+      attrValue,
+      i,
+      len;
+  for (i = 0, len = element.attributes.length; i < len; i++) {
+    attrName = element.attributes[i].nodeName;
+    attrValue = element.attributes[i].nodeValue;
+    if(element.attributes[i].specified) {
+      pairs.push(attrName + " = \"" + attrValue + "\"");
+    }
+  }
+  return pairs.join("、");
+}
+
+console.log(outputAttributes(u1));
+
