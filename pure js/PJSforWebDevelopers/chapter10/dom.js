48d3beb37c6b08cf7b7a11d0f4e52a33cd3a7537
XuTianZhu <493512132@qq.com>
2017-03-22 15:30:57 +0800

    About DOM. It's said that it's a high expense to operate DOM & access NodeList could lead to a lot of problems. You need to be careful.

--- pure js/PJSforWebDevelopers/chapter10/dom.js
+++ pure js/PJSforWebDevelopers/chapter10/dom.js
@@ -1,3 +1,8 @@
+var start_time = Date.now();
+
+/* ======== Main: DOM ======== */
+
+
 var d1 = document.getElementById("d1");
 
 var d2 = document.getElementById("d2");
@@ -30,4 +35,54 @@ function outputAttributes(element) {
 }
 
 console.log(outputAttributes(u1));
+
+// var ddv = document.createElement
+var ddv = document.getElementById("ddv");
+var textNode = document.createTextNode("B & <strong></strong> > < \"<strong>SXS</strong>");
+ddv.appendChild(textNode);
+ddv.childNodes[3].splitText(26);
+
+
+
+// 俩函数，动态加载 js、css
+
+function loadScriptString(code) {
+  var script = document.createElement("script");
+  script.type = "text/javascript";
+  try {
+    script.appendChild(document.createTextNode(code));
+  } catch(e) {
+    script.text = code;
+  }
+  document.body.appendChild(script);
+}
+
+
+
+function loadStyleString(css) {
+  var style = document.createElement("style");
+  style.type = "text/css";
+  try {
+    style.appendChild(document.createTextNode(css));
+  } catch(e) {
+    style.styleSheet.cssText = css;
+  }
+  var head = document.getElementsByTagName("head")[0];
+  head.appendChild(style);
+}
+
+
+
+
+
+/* ======== Main End ======== */
+
+var stop_time = Date.now();
+
+console.log("this script last: " + (stop_time - start_time) + "ms");
+
+start_time = null;
+stop_time = null;
+
+
 
